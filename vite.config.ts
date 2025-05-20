363c8d417f021f3cd5a6aa61380f0538d79bc584
tlwaller99 <trey.waller@powershades.com>
2025-05-20 13:49:10 -0500

    more title fixing

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/TLW-Brag/',
+  base: '/tlw-brag/', // <-- use all lowercase to match your repo/folder name
   plugins: [react()],
 })
